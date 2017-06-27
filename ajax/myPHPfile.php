a3d05e8fe0a36f862d8d412b5927961f9f37f502
ocpineda <oscarneedscoffee@gmail.com>
2017-06-27 00:17:57 -0500

    fixing spacing again. Sublime wasn't cooperating

--- ajax/myPHPfile.php
+++ ajax/myPHPfile.php
@@ -2,13 +2,13 @@
 
 class GetAjax {
 
-	public static function getButtonValue()
-	{
-		if( $_REQUEST["selectedButtonValue"] ) {
-			$buttonPHP = $_REQUEST['selectedButtonValue'];
-		  echo "Value button is ". $buttonPHP;
-		}
-	}
+  public static function getButtonValue()
+  {
+    if( $_REQUEST["selectedButtonValue"] ) {
+      $buttonPHP = $_REQUEST['selectedButtonValue'];
+      echo "Value button is ". $buttonPHP;
+    }
+  }
 
 }
 
