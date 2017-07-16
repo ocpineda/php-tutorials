98135b60cd1d2fde87e09d689a87e38c633c468c
ocpineda <oscarneedscoffee@gmail.com>
2017-07-15 23:29:52 -0500

    rename to getajax.php, add isset to

--- ajax/multiple-classes/getajax.php
+++ ajax/multiple-classes/getajax.php
@@ -4,23 +4,14 @@ include_once 'updatedstring.php';
 class GetAjax {
 
 public static function getButtonValue() {
-	if( $_REQUEST["selectedButtonValue"] )
+	if( isset($_REQUEST["selectedButtonValue"]) )
   	{
     	$buttonPHP = $_REQUEST['selectedButtonValue'];
-     	echo "Value button is ". $buttonPHP."<br />";
-     	
-     	//
-     	GetAjax::printThis($buttonPHP)." This is in getButtonValue()<br />";
-     	
-     	UpdatedString::printMe($buttonPHP);
+     	echo "Value button is ". $buttonPHP."<br />";     	
+     	echo UpdatedString::printMe($buttonPHP);
   	}
 }
 
-
-public static function printThis($str)  
-{
-	return "I am printThis() in class GetAjax! ".$str;
-}
 
 }
 
